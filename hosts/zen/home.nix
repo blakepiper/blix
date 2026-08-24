77ae4cf0f6d7e9a885ea12d08a317771d260f873
Blake Piper <blakepiper47@gmail.com>
2026-08-24 19:04:52 -0400

    Configure Zen Xfce HiDPI display

--- hosts/zen/home.nix
+++ hosts/zen/home.nix
@@ -1,4 +1,30 @@
-{ ... }:
+{ lib, pkgs, ... }:
+
+let
+  configureXfceDisplay = pkgs.writeShellApplication {
+    name = "blix-configure-zen-xfce-display";
+    runtimeInputs = [ pkgs.xrandr ];
+    text = ''
+      xrandr --output eDP-1 --mode 2880x1800 --rate 120
+    '';
+  };
+in
 {
-blix.monitors = [];
+  blix.monitors = [ ];
+
+  # The 14-inch 2880x1800 panel is about 242 PPI. Keep it at its native mode
+  # while presenting an effective 1440x900 desktop in Xfce.
+  xfconf.settings.xsettings = {
+    "Gdk/WindowScalingFactor" = lib.mkForce 2;
+    "Xft/DPI" = lib.mkForce 96;
+  };
+
+  xdg.configFile."autostart/blix-zen-xfce-display.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Configure Zen Xfce display
+    Exec=${configureXfceDisplay}/bin/blix-configure-zen-xfce-display
+    OnlyShowIn=XFCE;
+    X-GNOME-Autostart-enabled=true
+  '';
 }
